bc88767de41a4ffc72505c26ade219b5a0e79db3
Suman Saha <saha1.suman@gmail.com>
2020-03-10 20:49:15 -0500

    compute.py

--- module1.py
+++
@@ -1,4 +0,0 @@
-from azure.mgmt.network import NetworkManagementClient
-from azure.mgmt.compute import ComputeManagementClient
-from azure.common.client_factory import get_client_from_cli_profile
-
--- setup_azure_CMD.sh
+++ setup_azure_CMD.sh
@@ -11,6 +11,12 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 
 sudo apt-get update -y
-sudo apt-get install azure-cli -y
+sudo apt-get install azure-cli azure-cli-core -y
+
 az login 
 
+# Create azure environemnt 
+az group create -l eastus --n PyAzrg
+az network vnet create -g PyAzrg -n azure-pytest-vnet --address-prefix 10.0.0.0/16 --subnet-name azure-pytest-subnet --subnet-prefix 10.0.0.0/24
+az network public-ip create -g PyAzrg -n azpy1-ip --allocation-method Dynamic --version IPv4
+az network nic create -g PyAzrg --vnet-name azure-pytest-vnet --subnet azure-pytest-subnet -n pyaz-nic1 --public-ip-address azpy1-ip
