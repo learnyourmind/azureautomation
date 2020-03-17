c7b6a04697f80d1fceecb77e11d8601855d5a7a7
Suman Saha <saha1.suman@gmail.com>
2020-03-17 16:58:29 -0500

    Updated shell script

--- setup_azure_CMD.sh
+++ setup_azure_CMD.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#Set variables
+PROJECT_DIR="$HOME/projects/azure_python/azureautomation"
+
+
 # Upadate ubuntu and add gpg key
 sudo apt-get update -y sudo apt-get upgrade -y
 sudo apt-get install curl apt-transport-https lsb-release gpg -y
@@ -9,14 +13,28 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sud
 
 AZ_REPO=$(lsb_release -cs)            
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-
 sudo apt-get update -y
-sudo apt-get install azure-cli azure-cli-core -y
+
+# Installing tools
+pip show azure azure-cli-core azure-cli
+pip install azure virtualenv  
+
+#setup virtualenv
+mkdir $PROJECT_DIR && cd $PROJECT_DIR
+virtualenv mytestenv 
+cd $PROJECT_DIR/mytestenv/
 
 az login 
+
+# Lookup subscription status and default subscriptions
+az account list --output table
+az account set --subscription "Internal-susaha" 
+az ad sp create-for-rbac --sdk-auth
 
 # Create azure environemnt 
 az group create -l eastus --n PyAzrg
 az network vnet create -g PyAzrg -n azure-pytest-vnet --address-prefix 10.0.0.0/16 --subnet-name azure-pytest-subnet --subnet-prefix 10.0.0.0/24
 az network public-ip create -g PyAzrg -n azpy1-ip --allocation-method Dynamic --version IPv4
 az network nic create -g PyAzrg --vnet-name azure-pytest-vnet --subnet azure-pytest-subnet -n pyaz-nic1 --public-ip-address azpy1-ip
+python compute.py
+az vm list --resource-group PyAzrg
